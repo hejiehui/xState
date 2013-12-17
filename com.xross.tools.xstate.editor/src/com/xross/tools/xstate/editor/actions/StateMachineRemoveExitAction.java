8d2ec62cb770dcabf5441e4bfa4d60917851f87f
hejiehui <he_jiehui@163.com>
2013-12-17 23:45:18 +0800

    add actions

---
+++ com.xross.tools.xstate.editor/src/com/xross/tools/xstate/editor/actions/StateMachineRemoveExitAction.java
@@ -0,0 +1,37 @@
+package com.xross.tools.xstate.editor.actions;
+
+import org.eclipse.gef.ui.actions.WorkbenchPartAction;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.xross.tools.xstate.editor.StateMachineDiagramGraphicalEditor;
+import com.xross.tools.xstate.editor.model.Event;
+import com.xross.tools.xstate.editor.model.StateNode;
+
+public class StateMachineRemoveExitAction extends WorkbenchPartAction implements StateMachineActionConstants, StateMachineMessages{
+	private StateNode node;
+	public StateMachineRemoveExitAction(IWorkbenchPart part, StateNode node){
+		super(part);
+		setId(ID_PREFIX + CREATE_EVENT);
+		setText(CREATE_EVENT_MSG);
+		this.node = node;
+	}
+	
+	protected boolean calculateEnabled() {
+		return true;
+	}
+	
+	public void run() {
+		InputDialog dlg = new InputDialog(Display.getCurrent().getActiveShell(), "Create new Event: ", "Event", "event", null);
+		if (dlg.open() != Window.OK)
+			return;
+		String name = dlg.getValue();
+		
+		StateMachineDiagramGraphicalEditor editor = (StateMachineDiagramGraphicalEditor)getWorkbenchPart();
+		Event event = new Event();
+		event.setId(name);
+//		execute(new AddEventCommand(machine, event));
+	}
+}
