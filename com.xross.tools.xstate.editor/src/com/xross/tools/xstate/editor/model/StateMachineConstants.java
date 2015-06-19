dea6e67af322dca465ddc89da4b6918fcee1c17a
He, Jiehui <he_jiehui@163.com>
2015-06-19 14:46:25 +0800

    fix spelling

--- com.xross.tools.xstate.editor/src/com/xross/tools/xstate/editor/model/StateMachineConstants.java
+++ com.xross.tools.xstate.editor/src/com/xross/tools/xstate/editor/model/StateMachineConstants.java
@@ -11,7 +11,7 @@ public interface StateMachineConstants {
 	
 	String PROP_ID = "Id";
 	String PROP_ENTRY_ACTION = "Entry action";
-	String PROP_EXIT_ACTION = "Exist action";
+	String PROP_EXIT_ACTION = "Exit action";
 	String PROP_REFERENCE = "Reference state machine";
 	String PROP_DESRIPTION = "Description";
 	String PROP_EVENT = "Event";
