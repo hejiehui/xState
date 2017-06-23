97bfe4673afa136d43c049c4fd8429fc2ddaf062
He, Jiehui <he_jiehui@163.com>
2017-06-23 11:07:56 +0800

    new sample

---
+++ com.xrosstools.xstate.sample/src/main/java/com/xrosstools/xstate/sample/VipFlowTest.java
@@ -0,0 +1,31 @@
+package com.xrosstools.xstate.sample;
+
+import com.xrosstools.xstate.Event;
+import com.xrosstools.xstate.StateMachine;
+import com.xrosstools.xstate.StateMachineFactory;
+
+public class VipFlowTest {
+    public static void main(String[] args) {
+        try {
+            StateMachineFactory f = StateMachineFactory.load("sample.xstate");
+            StateMachine sm = f.create("vip flow");
+            
+            System.out.println("Start state: " + sm.getCurrentState().getId());
+            notify(sm, "pass");
+            notify(sm, "pass");
+            notify(sm, "pass");
+            notify(sm, "pass");
+            notify(sm, "pass");
+            
+            System.out.println("End state: " + sm.getCurrentState().getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    static void notify(StateMachine sm, String eventId) {
+        sm.notify(new Event(eventId));
+        System.out.println(sm.getCurrentState().getId());
+    }
+
+}
