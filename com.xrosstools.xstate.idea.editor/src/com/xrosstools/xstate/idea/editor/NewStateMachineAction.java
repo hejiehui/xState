3ca11053cb8d15bc03a284bfc16f3353ee232d80
He, Jiehui <he_jiehui@163.com>
2023-11-24 14:38:42 +0800

    optimize create model file action

--- com.xrosstools.xstate.idea.editor/src/com/xrosstools/xstate/idea/editor/NewStateMachineAction.java
+++ com.xrosstools.xstate.idea.editor/src/com/xrosstools/xstate/idea/editor/NewStateMachineAction.java
@@ -1,23 +1,15 @@
 package com.xrosstools.xstate.idea.editor;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.xrosstools.idea.gef.DefaultNewModelFileAction;
-
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 
 public class NewStateMachineAction extends DefaultNewModelFileAction {
     private static final String modelTypeName = "Xross State Machine";
     private static final String templatePath = "/template/emptyTemplate.xstate";
     private static final String newFileName = "new_xstate_file";
 
-    public NewStateMachineAction() {
-        super(modelTypeName, StateMachineFileType.EXTENSION, StateMachineIcons.STATE_MACHINE_DIAGRAM, newFileName, templatePath);
-    }
+    private static final String template = getTemplate(NewStateMachineAction.class, templatePath);
 
-    @Override
-    protected InputStream getResourceAsStream() {
-        return getClass().getResourceAsStream(templatePath);
+    public NewStateMachineAction() {
+        super(modelTypeName, StateMachineFileType.EXTENSION, StateMachineIcons.STATE_MACHINE_DIAGRAM, newFileName, template);
     }
 }
