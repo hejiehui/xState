c108df8c83cd2d2765af3209d0c65606ca52ce4c
He, Jiehui <he_jiehui@163.com>
2023-12-01 10:56:04 +0800

    remove unnecessary method

--- com.xrosstools.xstate.sample/src/main/java/com/xrosstools/xstate/sample/TestEnterAction.java
+++ com.xrosstools.xstate.sample/src/main/java/com/xrosstools/xstate/sample/TestEnterAction.java
@@ -4,10 +4,6 @@ import com.xrosstools.xstate.EntryAction;
 import com.xrosstools.xstate.Event;
 
 public class TestEnterAction implements EntryAction {
-    public void transit(String sourceStateId,String targetStateId, Event event) {
-        System.out.println(String.format("Transit from %s to %s on %s", sourceStateId, targetStateId, event.getId()));
-    }
-
     public void enter(String targetStateId, Event event) {
         System.out.println(String.format("Now enter from [%s] to [%s]", event.getId(), targetStateId));
     }
