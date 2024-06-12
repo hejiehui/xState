c106c93b116b253e9bde8c554e295b14d4294bb8
He, Jiehui <he_jiehui@163.com>
2024-06-13 00:15:14 +0800

    Release 1.3.2
    Transfer state machine name into class name with camel case style

--- com.xrosstools.xstate.idea.editor/src/com/xrosstools/xstate/idea/editor/actions/GenerateHelperAction.java
+++ com.xrosstools.xstate.idea.editor/src/com/xrosstools/xstate/idea/editor/actions/GenerateHelperAction.java
@@ -1,5 +1,12 @@
 package com.xrosstools.xstate.idea.editor.actions;
 
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.SourceFolder;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.xrosstools.idea.gef.actions.Action;
 import com.xrosstools.idea.gef.commands.Command;
@@ -16,7 +23,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.ZonedDateTime;
 
-public class GenerateHelpertsAction extends Action implements StateMachineMessages {
+public class GenerateHelperAction extends Action implements StateMachineMessages {
     private static final String CREATE_MACHINE =
             "    public static class %s {\n" +//state machine name, constants
             "%s" +
@@ -38,24 +45,27 @@ public class GenerateHelpertsAction extends Action implements StateMachineMessag
     private static final String STATE_DEF_HEADER =
             "        /*  State Constants */\n";
 
+    private Project project;
     private VirtualFile file;
     private StateMachineDiagram diagram;
     private Accessor<String> accessor;
     private ImplementationFinder finder;
 
-    public GenerateHelpertsAction(VirtualFile file, StateMachineDiagram diagram){
+    public GenerateHelperAction(Project project, VirtualFile file, StateMachineDiagram diagram){
+        this.project = project;
         this.file = file;
         this.diagram = diagram;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        StringBuffer codeBuf = getTemplate("/template/ConstantTemplate.txt");
-        replace(codeBuf, "!PACKAGE!", diagram.getHelperPackage());
-        replace(codeBuf, "!DESCRIPTION!", diagram.getDescription());
+        StringBuffer codeBuf = getTemplate("/template/HelperTemplate.txt");
+        replace(codeBuf, "!PACKAGE!", getValue(diagram.getHelperPackage()));
+        replace(codeBuf, "!DESCRIPTION!", getValue(diagram.getDescription()));
         replace(codeBuf, "!LAST_GENERATE_TIME!", ZonedDateTime.now().toString());
-        replace(codeBuf, "!TEST_CLASS!", toCamelCase(diagram.getName()));
-        replace(codeBuf, "!MODEL_PATH!", file.getPath());
+        replace(codeBuf, "!TEST_CLASS!", toClassName(diagram.getName()));
+        replace(codeBuf, "!MODEL_PATH!", findResourcesPath());
+
         replace(codeBuf, "!STATE_MACHINE_DEFINITIONS!", "\n" + generateBody().toString());
 
         new CodeDisplayer("Generated helper", codeBuf.toString()).show();
@@ -78,7 +88,7 @@ public class GenerateHelpertsAction extends Action implements StateMachineMessag
             }
 
             appendDesc(buf, MACHINE_COMMENTS, sm.getDescription());
-            String createMachine = String.format(CREATE_MACHINE, sm.getName(), buf.toString(), sm.getName());
+            String createMachine = String.format(CREATE_MACHINE, toClassName(sm.getName()), buf.toString(), sm.getName());
             constants.append(createMachine);
         }
 
@@ -94,7 +104,7 @@ public class GenerateHelpertsAction extends Action implements StateMachineMessag
     private void appendLabelId(StringBuffer buf, String label, String id) {
         label = label.contains(" ") ? label.replace(' ', '_') : label;
 
-        buf.append(String.format(CONSTANT_DEF, label, id));
+        buf.append(String.format(CONSTANT_DEF, label.toUpperCase(), id));
     }
 
     public Command createCommand() {
@@ -105,7 +115,7 @@ public class GenerateHelpertsAction extends Action implements StateMachineMessag
     public static StringBuffer getTemplate(String filePath){
         StringBuffer codeBuf = new StringBuffer();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(GenerateHelpertsAction.class.getResourceAsStream(filePath)));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(GenerateHelperAction.class.getResourceAsStream(filePath)));
         String line;
         try {
             while((line = reader.readLine()) != null)
@@ -129,11 +139,12 @@ public class GenerateHelpertsAction extends Action implements StateMachineMessag
         }
     }
 
-    public static String toVariable(String label) {
+    public static String toConstantName(String label) {
+        label = label.toUpperCase();
         return label.contains(" ") ? label.replace(' ', '_') : label;
     }
 
-    public static String toCamelCase(String label) {
+    public static String toClassName(String label) {
         StringBuffer clazz = new StringBuffer();
         for(String s: label.split(" ")) {
             clazz.append(capitalize(s));
@@ -147,5 +158,30 @@ public class GenerateHelpertsAction extends Action implements StateMachineMessag
             charArray[0] = Character.toUpperCase(charArray[0]);
         }
         return new String(charArray);
+    }
+
+    public static String getValue(String value) {
+        return value == null? "" : value;
+    }
+
+    private String findResourcesPath() {
+        for (Module module : ModuleManager.getInstance(project).getModules()) {
+            for (ContentEntry contentEntry : ModuleRootManager.getInstance(module).getContentEntries()) {
+                for (SourceFolder sourceFolder : contentEntry.getSourceFolders()) {
+                    if(sourceFolder.getFile() == null)
+                        continue;
+
+                    if (VfsUtilCore.isAncestor(sourceFolder.getFile(), file, false)) {
+                        if (sourceFolder.isTestSource() || sourceFolder.getFile().getPath().contains("resources")) {
+                            String resourceRoot = sourceFolder.getFile().getPath();
+                            return file.getPath().substring(resourceRoot.length() + 1);//Started with '/'
+                        }else{
+                            return file.getPath();
+                        }
+                    }
+                }
+            }
+        }
+        return file.getPath();
     }
 }
--- com.xrosstools.xstate.idea.editor/src/com/xrosstools/xstate/idea/editor/actions/GenerateTestAction.java
+++ com.xrosstools.xstate.idea.editor/src/com/xrosstools/xstate/idea/editor/actions/GenerateTestAction.java
@@ -1,6 +1,5 @@
 package com.xrosstools.xstate.idea.editor.actions;
 
-import com.intellij.openapi.ui.Messages;
 import com.xrosstools.idea.gef.actions.Action;
 import com.xrosstools.idea.gef.commands.Command;
 import com.xrosstools.xstate.idea.editor.model.StateMachine;
@@ -9,17 +8,17 @@ import com.xrosstools.xstate.idea.editor.model.StateNode;
 import com.xrosstools.xstate.idea.editor.model.StateTransition;
 
 import java.awt.event.ActionEvent;
-import static com.xrosstools.xstate.idea.editor.actions.GenerateHelpertsAction.*;
+import static com.xrosstools.xstate.idea.editor.actions.GenerateHelperAction.*;
 
 public class GenerateTestAction extends Action {
     private static final String MACHINE_HEADER =
-            "        /*  %s Test Cases */\n";//State machine name
+            "    /*  \"%s\" test cases */\n";//State machine name
 
     private static final String MACHINE_COMMENTS =
             "    //%s\n";
 
     private static final String NODE_HEADER =
-            "        /*  Test all output transitions of %s */\n";//State machine name
+            "    /*  Test all output transitions of %s */\n";//State machine name
 
     private static final String TRANSITION_CASE_HEADER =
             "    @Test\n" +
@@ -48,47 +47,52 @@ public class GenerateTestAction extends Action {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        StringBuffer classBuf = GenerateHelpertsAction.getTemplate("/template/TestCaseTemplate.txt");
-        replace(classBuf, "!PACKAGE!", diagram.getHelperPackage());
-        replace(classBuf, "!TEST_CLASS!", toCamelCase(diagram.getName()));
+        StringBuffer classBuf = GenerateHelperAction.getTemplate("/template/TestCaseTemplate.txt");
+        replace(classBuf, "!PACKAGE!", getValue(diagram.getHelperPackage()));
+        replace(classBuf, "!TEST_CLASS!", toClassName(diagram.getName()));
 
         StringBuffer bodyBuf = new StringBuffer();
         for(StateMachine sm: diagram.getMachines()) {
-            String machine = toVariable(sm.getName());
-            String Machine = capitalize(machine);
-            bodyBuf.append(String.format(MACHINE_HEADER, machine));
+            String Machine = toClassName(sm.getName());
+            bodyBuf.append(String.format(MACHINE_HEADER, sm.getName()));
             appendDesc(bodyBuf, MACHINE_COMMENTS, sm.getDescription());
 
             for(StateNode stateNode: sm.getNodes()) {
-                String state = toVariable(stateNode.getDisplayText());
-                String State = capitalize(state);
+                String stateName = stateNode.getDisplayText();
+                String state = toConstantName(stateName);
+                String State = toClassName(stateName);
 
                 if(stateNode.getOutputs().size() != 0)
                     bodyBuf.append(String.format(NODE_HEADER, stateNode.getDisplayText()));
 
                 for(StateTransition tran: stateNode.getOutputs()) {
-                    String event = toVariable(tran.getEvent().getDisplayText());
-                    String Event = capitalize(event);
+                    String eventName = tran.getEvent().getDisplayText();
+                    String event = toConstantName(eventName);
+                    String Event = toClassName(eventName);
                     String guard = tran.getTransitGuard() == null || tran.getTransitGuard().trim().length() == 0 ? null : tran.getTransitGuard().trim();
 
                     bodyBuf.append(String.format(TRANSITION_CASE_HEADER, Machine, State, Event, ""));
 
                     if(guard !=null)
                         bodyBuf.append(String.format(GUARD_PASS_COMMENT, tran.getTransitGuard()));
-                    bodyBuf.append(String.format(TRANSITION_CASE_BODY, machine, state, event, tran.getTarget().getDisplayText()));
+                    bodyBuf.append(String.format(TRANSITION_CASE_BODY, Machine, state, event, toConstantName(tran.getTarget().getDisplayText())));
 
                     if(guard !=null) {
-                        bodyBuf.append(String.format(GUARD_PASS_COMMENT, tran.getTransitGuard()));
                         bodyBuf.append(String.format(TRANSITION_CASE_HEADER, Machine, State, Event, "Fail"));
                         bodyBuf.append(String.format(GUARD_FAIL_COMMENT, tran.getTransitGuard()));
 
                         //The next should be the same as original state
-                        bodyBuf.append(String.format(TRANSITION_CASE_BODY, machine, state, event, state));
+                        bodyBuf.append(String.format(TRANSITION_CASE_BODY, Machine, state, event, state));
                     }
                 }
             }
         }
-        replace(classBuf, "!TEST_BODY!", bodyBuf.toString());
+
+        String codeBody = bodyBuf.toString();
+        if(codeBody.endsWith("\n\n"))
+            codeBody = codeBody.substring(0, codeBody.length()-2);
+
+        replace(classBuf, "!TEST_BODY!", codeBody);
 
         new CodeDisplayer("Generated test", classBuf.toString()).show();
     }
